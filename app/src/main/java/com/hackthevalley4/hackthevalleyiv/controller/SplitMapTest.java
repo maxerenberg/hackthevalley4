0328ef2763ff1a0d0d1e4c243e1d78ae902bfe44
f2kwan <f2kwan@edu.uwaterloo.ca>
2020-02-15 16:35:00 -0500

    Test files added

---
+++ app/src/main/java/com/hackthevalley4/hackthevalleyiv/controller/SplitMapTest.java
@@ -0,0 +1,16 @@
+package com.hackthevalley4.hackthevalleyiv.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackthevalley4.hackthevalleyiv.R;
+
+public class SplitMapTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_map_test);
+    }
+}
