ec413224a1c6e27bc586c34eefbb522c834adb0f
f2kwan <f2kwan@edu.uwaterloo.ca>
2020-02-15 14:47:39 -0500

    Setup account create

--- app/src/main/java/com/hackthevalley4/hackthevalleyiv/controller/AccountCreateActivity.java
+++ app/src/main/java/com/hackthevalley4/hackthevalleyiv/controller/AccountCreateActivity.java
@@ -3,16 +3,28 @@ package com.hackthevalley4.hackthevalleyiv.controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.hackthevalley4.hackthevalleyiv.R;
 
 public class AccountCreateActivity extends AppCompatActivity {
 
-    
+    private EditText name;
+    private EditText email;
+    private EditText username;
+    private EditText password;
+    private EditText preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_create);
+        name = findViewById(R.id.createActName);
+        email = findViewById(R.id.createActEmail);
+        username = findViewById(R.id.createActUsername);
+        password = findViewById(R.id.createActPassword);
+        preferences = findViewById(R.id.createActPreferences);
+
+
     }
 }
